634b4b3d60424b5b963dbfc80d4cd19cf2a93d31
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-03-20 12:09:52 +0900

    `webpack-dev-server`パッケージを追加

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,11 @@ module.exports = {
     maxAssetSize: 1_000_000,
     maxEntrypointSize: 1_000_000,
   },
+  // ref: https://zenn.dev/sa2knight/articles/9b19ffd391bca87d7b8c
+  devServer: {
+    hot: true,
+    static: {
+      directory: `${__dirname}/src`,
+    },
+  },
 };
